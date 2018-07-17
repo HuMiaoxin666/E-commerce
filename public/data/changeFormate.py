e3511b197dc4d27c9215075f6326c00d200acde0
HuMiaoxin666 <humiaoxin@gmail.com>
2018-07-17 17:21:21 +0800

    Comlete data fresh!

--- public/data/changeFormate.py
+++ public/data/changeFormate.py
@@ -1,20 +1,25 @@
 import csv
+f_or = open("decl_po_001.csv", 'r', encoding='utf-8')
+f_infor = open("orderInfor.csv", 'r', encoding='utf-8')
 
-with open("result2.csv", 'r', encoding='utf-8') as f:
-    data = csv.reader(f)
-    outfile = open("orderInfor.csv", 'w', newline='', encoding='utf-8')
-    count = 0
-    for row in data:
-        if count != 0:
-            curp = row[-1].split(' ')
-            out_row = row[0:-1]
-            [out_row.append(p) for p in curp]
-            csv.writer(outfile).writerow(out_row)
-        else:
-            out_row = row[0:-1]
-            out_row.append("lng")
-            out_row.append("lat")
-            csv.writer(outfile).writerow(out_row)
-        count += 1
-    outfile.close()
+data_or = csv.reader(f_or)
+time_dict = {}
+for row in data_or:
+    time_dict[row[0]] = [row[2], row[-1]]
+data_infor = csv.reader(f_infor)
+count = 0
+outfile = open("newInfor.csv", 'w', newline='', encoding = 'utf-8')
 
+for row in data_infor:
+    if count > 0:
+        cur_time = time_dict[row[0]]
+        for t in cur_time:
+            row.append(t)
+        print(row)
+        csv.writer(outfile).writerow(row)
+    else:
+        row.append("startTime")
+        row.append("endTime")
+        csv.writer(outfile).writerow(row)
+    count += 1
+outfile.close()
--- public/js/options.js
+++ public/js/options.js
@@ -1,20 +1,21 @@
 var options = (function () {
-    var WhStatus = "wh_5";//判断当前被选中的仓库
-    var CgStatus = "cg_1";//判断当前被选中的种类
-    var WH_index = WhStatus[WhStatus.length-1];
+    var WhStatus = "wh_5"; //判断当前被选中的仓库
+    var CgStatus = "cg_1"; //判断当前被选中的种类
+    var WH_index = WhStatus[WhStatus.length - 1];
     var CG_index = parseInt(CgStatus[CgStatus.length - 1]) - 1;
-    var cgName = ["服饰鞋靴","环球美食","家居个护","美容彩妆","母婴儿童","营养保健"];
+    var cgName = ["服饰鞋靴", "环球美食", "家居个护", "美容彩妆", "母婴儿童", "营养保健"];
     //添加仓库悬浮事件和点击事件
     for (var i = 1; i <= 6; i++) {
         $("#wh_" + String(i)).click(function () {
-            for (var i = 1; i <=6; i++)
+            for (var i = 1; i <= 6; i++)
                 $("#wh_" + String(i)).css("color", "black");
             $("#" + this.id).css("color", "rgb(0, 123, 255)");
             WhStatus = ($("#" + this.id).css("color") == "rgb(0, 123, 255)") ? this.id : false;
             console.log("Searching !")
             console.log('cgName[CgStatus]: ', CG_index);
             console.log('WhStatus: ', WH_index);
-            mapView.getChosenData(WH_index, cgName[CG_index]).then(function(data){
+            mapView.getChosenData(WH_index, cgName[CG_index]).then(function (data) {
+                AddOptions(data);
                 console.log('data: ', data);
             });
             console.log("Ok !")
@@ -34,13 +35,14 @@ var options = (function () {
 
     for (var i = 1; i <= 6; i++) {
         $("#cg_" + String(i)).click(function () {
-            for (var i = 1; i <=6; i++)
+            for (var i = 1; i <= 6; i++)
                 $("#cg_" + String(i)).css("color", "black");
             $("#" + this.id).css("color", "rgb(0, 123, 255)");
             CgStatus = ($("#" + this.id).css("color") == "rgb(0, 123, 255)") ? this.id : false;
             CG_index = parseInt(CgStatus[CgStatus.length - 1]) - 1;
             console.log(cgName[CG_index]);
-            mapView.getChosenData(WH_index, cgName[CG_index]).then(function(data){
+            mapView.getChosenData(WH_index, cgName[CG_index]).then(function (data) {
+                AddOptions(data);
                 console.log('data: ', data);
             });
         });
@@ -53,13 +55,40 @@ var options = (function () {
                 $("#" + this.id).css("color", "black");
         });
     }
+    //添加订单下拉框元素
+    function AddOptions(chosenData) {
+        $("#orderNum").empty();
+        for (var i = 0; i < chosenData.length; i++) {
+            var options = $('<option>3</option>').text(chosenData[i]["order_no"]).attr("id",i);
+            $("#orderNum").append(options);
+        }
+        //添加下拉框选中事件
+        var cgId = ['id','warehouse','startTime','endTime','name','qty','address']
+        $("#orderNum").on("click",function(){
+            let cur_orderNum = $("#orderNum").find("option:selected").text();
+            let cur_orderInfor;
 
-    var warehouse = '5';
-    var type = '浙江省';
+            for (var i = 0; i < chosenData.length; i++) {
+                console.log('chosenData[i].order_no: ', chosenData[0].order_no);
+                if(chosenData[i].order_no == cur_orderNum){
+                    
+                    cur_orderInfor = chosenData[i];
+                    console.log("yes");
+                }
+            }
+            console.log('cur_orderInfor: ', cur_orderInfor);
+            for(var i = 0; i < cgId.length -1; i++){
+                $("#" + cgId[i]).val(cur_orderInfor[cgId[i]]);
+            }
+            var cur_address = cur_orderInfor['province'] + ' '+ cur_orderInfor['city']  +' '+cur_orderInfor['country'];
+            $("#address").val(cur_address);
+        })
+    }
+   
+
     return {
-        WhStatus:WhStatus,
-        CgStatus:CgStatus,
-        warehouse: warehouse,
+        WhStatus: WhStatus,
+        CgStatus: CgStatus,
         type: cgName[CgStatus - 1]
     };
 })();
